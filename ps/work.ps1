ad1fef0235a9399d2ae226b5402e7a16a4d96765
jim steffes <jimsteffes@outlook.com>
2021-11-29 15:46:28 -0600

    ok

--- ps/work.ps1
+++ ps/work.ps1
@@ -1,23 +1,17 @@
-# foreach ($file in Get-ChildItem)
-# {
-#   if ($file.length -gt 100KB)
-#   {
-#     Write-Host $file
-#     Write-Host $file.length
-#     Write-Host $file.lastaccesstime
-#   }
-# }
 
 # $searchPattern= Read-Host -Prompt "Search pattern: "
 # $c="*"+ $searchPattern  +"*"
 # Get-ChildItem $c
 $twitString= Read-Host -Prompt "File name: "
 # $twitString='song'
-for (($i = 0); $i -lt 3; $i++)
+for (($i = 0); $i -lt 7; $i++)
 {
     # "`$i:$i" + ":      `$j:$j" + "write jim   "
     $str='ffmpeg -i "concat:' +"$twitString$i.mp3|$twitString$i.mp3`" -acodec copy "+$twitString+($i+1)+".mp3"
+    Write-Host($str)
+    # Exit
     Invoke-Expression($str)
+    # Remove-Item $twitString+($i)+".mp3"
     Write-Host($str)
     # $i.GetType()
 }
